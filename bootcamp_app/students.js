a043ff7fd99ef48a19f73eee1ff77da410e9b2d7
Jeff Gebert <jeffgebert@hotmail.com>
2019-11-14 01:53:25 +0000

    updated student.js and teachers.js with Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,6 @@
 let month = process.argv[2];
 let limit = process.argv[3];
+const values = [`%${month}%`, limit];
 
 const { Pool } = require('pg');
 
@@ -15,9 +16,9 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${month}%'
-LIMIT ${limit};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`,values)
 .then(res => {
   for (let x = 0; x <res.rows.length; x++) {
     console.log(res.rows[x].name + " has an id of " + res.rows[x].student_id + " and was in the " + res.rows[x].cohort + " cohort")
